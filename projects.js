c21a467e03bb8d27c12ccceaf584db35c9f75e44
Matt Logan <mlogan5212@gmail.com>
2020-04-24 10:42:39 -0500

    Started JS projects Object

--- projects.js
+++ projects.js
@@ -1,1 +1,29 @@
-console.log('Test');
+const projects = [
+  {
+    title: 'Product Cards',
+    screenshot: '',
+    description: 'Our first project at NSS was to create product cards using HTML and CSS. The biggest challenge of this project was placing the sold out image over the card.',
+    technologiesUsed: 'HTML, CSS, Version Control with GitHub ',
+    available: true,
+    url: '',
+    githubUrl: 'https://github.com/MLogan1998/product-cards'
+  },
+  {
+    title: 'CSS Theme Challenge',
+    screenshot: '',
+    description: 'This was an opttional challenge to recreate a template from TEMPLATED. At the time, by far the most advanced work I had done. I think it turned out pretty good!',
+    technologiesUsed: 'HTML, CSS, Version Control with GitHub ',
+    available: true,
+    url: '',
+    githubUrl: 'https://github.com/MLogan1998/css-theme-challenge'
+  },
+  {
+    title: 'Pet Adoption',
+    screenshot: '',
+    description: 'Create pet cards using JavaScript. This was our first project using JavaScript to print to the dom. Added buttons to filter the cards by animal type using jQuery.',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, jQuery, Version Control with GitHub ',
+    available: true,
+    url: '',
+    githubUrl: 'https://github.com/MLogan1998/pet-adoption'
+  },
+]
