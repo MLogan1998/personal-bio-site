c9b7d15cbb30cbf5621f75ea98bdcaa3e8263a28
Matt Logan <mlogan5212@gmail.com>
2020-04-26 16:12:35 -0500

    comleted technologies section and footer

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ const projects = [
     camel: 'projectCards',
     screenshot: 'https://i.postimg.cc/vmzTV7r6/product-cards-final.jpg',
     description: 'Our first project at NSS was to create product cards using HTML and CSS. The biggest challenge of this project was placing the sold out image over the card.',
-    technologiesUsed: 'HTML, CSS, GitHub ',
+    technologiesUsed: 'HTML, CSS',
     available: true,
     url: '',
     githubUrl: 'https://github.com/MLogan1998/product-cards'
@@ -14,7 +14,7 @@ const projects = [
     camel: 'cssThemeChallenge',
     screenshot: 'https://i.postimg.cc/LXkjbZFt/css-theme-challenge-final.jpg',
     description: 'This was an opttional challenge to recreate a template from TEMPLATED. At the time, by far the most advanced work I had done. I think it turned out pretty good!',
-    technologiesUsed: 'HTML, CSS, GitHub ',
+    technologiesUsed: 'HTML, CSS',
     available: true,
     url: '',
     githubUrl: 'https://github.com/MLogan1998/css-theme-challenge'
@@ -24,7 +24,7 @@ const projects = [
     camel: 'petAdoption',
     screenshot: 'https://i.postimg.cc/Kz44b9Xn/pet-adoption-final2.jpg',
     description: 'Create pet cards using JavaScript. This was our first project using JavaScript to print to the dom. Added buttons to filter the cards by animal type.',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, GitHub ',
+    technologiesUsed: 'HTML, CSS, Vanilla JS',
     available: true,
     url: '',
     githubUrl: 'https://github.com/MLogan1998/pet-adoption'
